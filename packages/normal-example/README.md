ff5ada86e5d34c11f6b5c80a976656e542eb09a3
yzc <qiuzhch@yonyou.com>
2025-10-16 15:13:40 +0800

    chore: add normal example

---
+++ packages/normal-example/README.md
@@ -0,0 +1,11 @@
+# `normal-example`
+
+> TODO: description
+
+## Usage
+
+```
+const normalExample = require('normal-example');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/normal-example/__tests__/normal-example.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const normalExample = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(normalExample(), 'Hello from normalExample');
+console.info('normalExample tests passed');
